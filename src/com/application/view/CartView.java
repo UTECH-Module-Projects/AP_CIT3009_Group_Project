e83dbbc5c15bb5e6a5d1d2c7e75360c2eb22dc74
Gabbiij <johnsongabrielle123@gmail.com>
2022-11-09 01:58:36 -0500

    manager view commit and updated cart view

--- src/com/application/view/CartView.java
+++ src/com/application/view/CartView.java
@@ -4,7 +4,10 @@ import net.miginfocom.swing.MigLayout;
 
 import javax.swing.*;
 import javax.swing.border.Border;
+import javax.swing.table.DefaultTableModel;
 import java.awt.*;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
 import java.awt.event.KeyAdapter;
 import java.awt.event.KeyEvent;
 import java.text.SimpleDateFormat;
@@ -12,9 +15,16 @@ import java.util.Calendar;
 
 public class CartView {
     private JFrame frame;
-    private JLabel item, description, quantity, price, total, shopBag, lock, border;
+    private JLabel item, quantity, price, total, shopBag, lock, border;
     private JLabel head1, head2, head3, phoneNum, date, time, InvoiceNum, custID, message, disco, tot, compEmail;
     private JTextField itemTxt;
+    private JTable table;
+    private JScrollPane scroll;
+    private JTextArea Area;
+    private JButton bill;
+    String[] columns = {"ITEMS", "QTY", "PRICE", "DISC%", "TOTAL"};
+    String[][] data = {{"egg", "3", "98", "-", "234"},
+            {"bread", "1", "61", "y", "40"}};
 
 
     Calendar calendar;
@@ -23,11 +33,11 @@ public class CartView {
     private String timer, day;
 
 
-    public CartView(){
+    public CartView() {
         try {
             UIManager.setLookAndFeel("javax.swing.plaf.nimbus.NimbusLookAndFeel");
         } catch (UnsupportedLookAndFeelException | ClassNotFoundException | InstantiationException |
-                IllegalAccessException e) {
+                 IllegalAccessException e) {
             throw new RuntimeException(e);
         }
 
@@ -36,10 +46,10 @@ public class CartView {
         setWindowProperties();
     }
 
-    public void initializeComponents(){
+    public void initializeComponents() {
         frame = new JFrame("Welcome to Jan's Wholesale & Retail");
         //frame.setLayout(new MigLayout("insets 15, debug, fill", "[][]", "[][][]"));
-        frame.setLayout(new MigLayout(" insets 40 60 20 60, fillx", "[][]", " [] [] [] []"));
+        frame.setLayout(new MigLayout(" debug, insets 40 60 20 60, fillx", "[][][]", "[][][]"));
 
         //Address
         head1 = new JLabel("JAN'S WHOLESALE & RETAIL", JLabel.CENTER);
@@ -81,22 +91,20 @@ public class CartView {
         custID = new JLabel("Customer ID: ");
         custID.setForeground(Color.white);
 
-        ImageIcon icon = new ImageIcon("C:\\Users\\johns\\IdeaProjects\\WholesaleAP\\src\\Image\\cart.png");
+        ImageIcon icon = new ImageIcon("src\\Image\\cart.png");
         shopBag = new JLabel("YOUR RECEIPT ", JLabel.CENTER);
         shopBag.setForeground(Color.white);
         shopBag.setIcon(icon);
 
         message = new JLabel("Thanks for shopping with Jan :)");
         message.setForeground(Color.white);
-        message.setBounds(0, 300, 0,0 );
+        message.setBounds(0, 300, 0, 0);
 
         quantity = new JLabel("QTY");
         quantity.setForeground(Color.white);
 
-
         item = new JLabel("ITEMS ");
         item.setForeground(Color.white);
-        itemTxt = new JTextField(10);
 
         price = new JLabel("PRICE");
         price.setForeground(Color.white);
@@ -107,9 +115,43 @@ public class CartView {
         tot = new JLabel("TOTAL");
         tot.setForeground(Color.white);
 
+        table = new JTable(data, columns);
+        scroll = new JScrollPane(table);
+        table.setShowGrid(false);
+        table.setSize(100, 100);
+
+        bill = new JButton("Bill");
+        Area = new JTextArea();
+
+        bill.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+                Area.setText(Area.getText() + "=================================\n");
+                Area.setText(Area.getText() + "\tJan's Wholesale\n");
+                Area.setText(Area.getText() + "=================================\n");
+                Area.setText(Area.getText() + "ITEMS" + "\t" + "QTY" + "\t" + "PRICE" + "\t" + "DISC%" + "\t" + "TOTAL");
+                for (int i = 0; i < table.getRowCount(); i++) {
+                    String items = table.getValueAt(i, 0).toString();
+                    String qty = table.getValueAt(i, 1).toString();
+                    String pri = table.getValueAt(i, 2).toString();
+                    String disc = table.getValueAt(i, 3).toString();
+                    String total = table.getValueAt(i, 4).toString();
+                    Area.setText(Area.getText() + "\n\n");
+                    Area.setText(Area.getText() + items + "\t" + qty + "\t" + pri + "\t" + disc + "\t" + total);
+                }
+                Area.setText(Area.getText() + "\n\n");
+
+                try {
+                    Area.print();
+                } catch (Exception ex) {
+                    ex.printStackTrace();
+                }
+            }
+        });
+
 
         //Icon
-        ImageIcon img = new ImageIcon("C:\\Users\\johns\\IdeaProjects\\WholesaleAP\\src\\Image\\Jan.png");
+        ImageIcon img = new ImageIcon("src/Image/Jan.png");
         frame.setIconImage(img.getImage());
 
         //Font
@@ -142,32 +184,10 @@ public class CartView {
         disco.setFont(newFont2);
         tot.setFont(newFont2);
 
-        description = new JLabel("Description");
-
-
-
-       /* border= new JLabel();
-        Border LineBorder = BorderFactory.createLineBorder(Color.CYAN);
-
-        border.setBorder(LineBorder);
-        */
 
     }
 
-   public void setTime() {
-     while(true){
-    timer = timeFormat.format(Calendar.getInstance().getTime());
-    timeLabel.setText(timer);
-        try{
-            Thread.sleep(1000);
-        }catch (InterruptedException e){
-            e.printStackTrace();
-        }
-
-     }
-    }
-
-    public void addComponentsToFrame(){
+    public void addComponentsToFrame() {
 /*
         //frame.add(title, " pushx, span, growx, wrap ");
         frame.add(title, "center, wrap, span, growx, dock north");
@@ -176,58 +196,57 @@ public class CartView {
         frame.add(description, "span, growx");*/
         frame.getContentPane().setBackground(new java.awt.Color(36, 36, 36));
 //heading
-        frame.add(head1,"north");
-        frame.add(head2,"north");
-        frame.add(head3,"north");
-        frame.add(phoneNum,"north");
-        frame.add(compEmail,"north");
+        frame.add(head1, "north");
+        frame.add(head2, "north");
+        frame.add(head3, "north");
+        frame.add(phoneNum, "north");
+        frame.add(compEmail, "north");
+
 
         //date
-        frame.add(date,"split");
+        frame.add(date, "split");
         frame.add(dateLabel, "split");
 
         //Customer ID Label
         frame.add(custID, " gapleft 66, wrap");
 
-
         //time
-        frame.add(time,"split");
-        frame.add(timeLabel ,"split");
+        frame.add(time, "split");
+        frame.add(timeLabel, "split");
 
         //Invoice label
         frame.add(InvoiceNum, " gapleft 60, wrap");
 
         //top labels
         frame.add(shopBag, " gapleft 20, split, gaptop 20, wrap");
-
-
-        frame.add(item," gaptop 20, split");
-        //frame.add(itemTxt, "pushx,  span");
+        /*frame.add(item," gaptop 20, split");
 
         frame.add( quantity, "gapleft 100, split");
 
         frame.add(price, "gapleft 20, split");
         frame.add(disco, "gapleft 20");
-        frame.add(tot, "gapleft 20");
+        frame.add(tot, "gapleft 20, wrap 20");*/
 
+        frame.add(scroll, "gaptop 10, span 3, wrap");
+
+        frame.add(bill);
 
         //frame.add(message,"south");
         //frame.add(border);
     }
 
 
-    public void setWindowProperties(){
+    public void setWindowProperties() {
         frame.setResizable(true);
         frame.setVisible(true);
         frame.setLocationRelativeTo(null);
-
         frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
         frame.pack();
         frame.setSize(600, 700);
 
     }
 
-    public static void main (String [] args){
+    public static void main(String[] args) {
         new CartView();
     }
 }
---
+++ src/com/application/view/ManageView.java
@@ -0,0 +1,99 @@
+package com.application.view;
+
+import net.miginfocom.swing.MigLayout;
+
+import javax.swing.*;
+import javax.swing.event.ListSelectionEvent;
+import javax.swing.event.ListSelectionListener;
+import java.awt.*;
+import java.awt.event.MouseEvent;
+import java.awt.event.MouseListener;
+
+public class ManageView {
+    private JFrame frame;
+    private JTable table;
+    private JTextArea Area;
+    private JScrollPane scroll1, scroll2;
+    private JButton select, clear;
+    private JPanel pan1, pan2;
+    String [] column ={"Invoice ID" ,"Bill Date", "Customer ID", "Employee ID"};
+    String [][] data = {{"20", "12/12/22", "83745", "77383"},{"47", "1/09/20", "84485", "94747"}};
+
+    public ManageView() {
+        try {
+            UIManager.setLookAndFeel("javax.swing.plaf.nimbus.NimbusLookAndFeel");
+        } catch (UnsupportedLookAndFeelException | ClassNotFoundException | InstantiationException |
+                 IllegalAccessException e) {
+            throw new RuntimeException(e);
+        }
+
+        initializeComponents();
+        addComponentsToFrame();
+        setWindowProperties();
+    }
+
+    public void  initializeComponents(){
+        frame = new JFrame("Jan's Wholesale and Retail");
+        frame.setLayout(new MigLayout(" fill", "[][]","[][][]"));
+
+        //table
+        table = new JTable(data, column);
+        scroll1 = new JScrollPane(table);
+        table.setShowGrid(false);
+
+        table.getSelectionModel().addListSelectionListener(new ListSelectionListener() {
+            @Override
+            public void valueChanged(ListSelectionEvent e) {
+                if (!e.getValueIsAdjusting() && table.getSelectedRow()!=-1) {
+
+                    String id = table.getValueAt(table.getSelectedRow(), 0).toString();
+                    String billdate = table.getValueAt(table.getSelectedRow(), 1).toString();
+                    String empID = table.getValueAt(table.getSelectedRow(), 2).toString();
+                    String custId = table.getValueAt(table.getSelectedRow(), 3).toString();
+
+                    Area.setText(Area.getText() + "\n\n");
+                    Area.setText(Area.getText() + id +"\t"+billdate+"\t"+empID +"\t" + custId);
+
+                }
+            }
+
+        });
+
+
+        //Area
+        Area = new JTextArea(30, 30);
+        scroll2 = new JScrollPane(Area);
+        Area.setEditable(false);
+
+        //button
+        select = new JButton("Select");
+        clear = new JButton("Clear");
+
+        //panels
+        pan1 = new JPanel();
+        pan2 = new JPanel();
+
+        pan1.add(scroll1);
+        pan2.add(scroll2);
+
+    }
+    public void addComponentsToFrame(){
+
+        frame.add(pan1," span 3, grow, split");
+        frame.add(pan2," gaptop 10, wrap, span 3");
+        frame.add(select, "split");
+        frame.add(clear);
+
+    }
+    public void setWindowProperties(){
+        frame.setResizable(true);
+        frame.setVisible(true);
+        frame.setLocationRelativeTo(null);
+        frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        frame.pack();
+        frame.setSize(800, 500);
+    }
+    public static void main (String[] args){
+        new ManageView();
+    }
+}
